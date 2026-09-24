0f64ee9cdf5e75ffafedc7124853edf683fb90de
zakzakst <e1433104@gmail.com>
2026-09-25 07:44:30 +0900

    fix

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 
 export default defineManifest({
   manifest_version: 3,
-  name: "Extension Template",
+  name: "INPUT FORM",
   version: "1.0.0",
 
   permissions: [
